7322a1a436ea4e02e04766b8ef173a94c4c316bb
manh20051151 <nguyenvietmanh1409@gmail.com>
2024-01-28 17:14:55 +0700

    undate index.js

--- src/index.js
+++ src/index.js
@@ -12,8 +12,11 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+// app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb' }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 routes(app);
